155b1d3d379cbfc09f7c5c8dce8bd48c14271fb5
bobowang2017 <anini456123>
2019-11-07 19:03:35 +0800

    init

--- apis/celery_task/tasks.py
+++ apis/celery_task/tasks.py
@@ -1,9 +1,9 @@
 from exts import celery
-# from celery.utils.log import get_task_logger
+from celery.utils.log import get_task_logger
 # # 创建一个logger对象
-# logger = get_task_logger('name')
+logger = get_task_logger(__name__)
 
 
 @celery.task(name='apis.celery_task.tasks.timer_print')
 def timer_print():
-    print('*' * 50)
+    logger.info('*' * 50)
--- celery_config.py
+++ celery_config.py
@@ -13,4 +13,5 @@ class CeleryConfig(object):
             "args": ()  # # 任务函数参数
         }
     }
+    CELERY_IMPORTS = ('apis.celery_task.tasks',)
     # https://docs.celeryproject.org/en/latest/userguide/configuration.html
