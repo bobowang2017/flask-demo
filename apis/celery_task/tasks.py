0cc5c864fc5c9ca621cae37e2248d979b8d10da6
bobowang2017 <anini456123>
2019-11-08 11:19:58 +0800

    init

--- apis/celery_task/tasks.py
+++ apis/celery_task/tasks.py
@@ -1,4 +1,4 @@
-from apis.project.models import Project
+
 from exts import celery_app
 from celery.utils.log import get_task_logger
 # # 创建一个logger对象
@@ -8,6 +8,7 @@ logger = get_task_logger(__name__)
 @celery_app.task
 def timer_print():
     logger.info('*' * 50)
+    from apis.project.models import Project
     projects = Project.query.all()
     for p in projects:
         logger.info(p.name)
