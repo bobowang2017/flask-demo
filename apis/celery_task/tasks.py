d7de3ca6a15dd42305ab72a67f41a2c6040abbf7
bobowang2017 <anini456123>
2019-11-08 11:44:53 +0800

    init

--- apis/celery_task/tasks.py
+++ apis/celery_task/tasks.py
@@ -1,5 +1,5 @@
 
-from exts import celery_app
+from exts import celery_app, db
 from celery.utils.log import get_task_logger
 # # 创建一个logger对象
 logger = get_task_logger(__name__)
@@ -10,7 +10,7 @@ def timer_print(app):
     logger.info('*' * 50)
     with app.app_context():
         from apis.project.models import Project
-        projects = Project.query.all()
+        projects = db.session.query(Project).all()
         for p in projects:
             logger.info(p.name)
             logger.info(p.code)
