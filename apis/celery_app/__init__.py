be046a77e87ffdb4b720b534aecaa47ba4087c62
bobowang2017 <anini456123>
2019-11-08 13:45:15 +0800

    init

--- apis/celery_app/__init__.py
+++ apis/celery_app/__init__.py
@@ -1,12 +1,5 @@
 from celery import Celery
-from celery.schedules import crontab
 
 celery_app = Celery()
 celery_app.config_from_object('celery_config.CeleryConfig')
-celery_app.conf.beat_schedule = {
-    'add-every-30-seconds': {
-        'task': 'apis.celery_task.tasks.timer_print',
-        'schedule': crontab(),
-        'args': ()
-    },
-}
+
---
+++ apis/celery_app/celery_config.py
@@ -0,0 +1,20 @@
+from datetime import timedelta
+
+from celery.schedules import crontab
+
+
+class CeleryConfig(object):
+    CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+    CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/1"
+    CELERY_TIMEZONE = 'Asia/Shanghai'
+    CELERY_ENABLE_UTC = True
+    # CELERYBEAT_SCHEDULE = {
+    #     "test1": {
+    #         "task": "apis.celery_task.tasks.timer_print",  # 执行的函数
+    #         # 'schedule': crontab(minute=0, hour=0),  # every minute 每分钟执行
+    #         'schedule': timedelta(seconds=20),  # every minute 每分钟执行
+    #         "args": ()  # # 任务函数参数
+    #     }
+    # }
+    CELERY_IMPORTS = ('apis.celery_app.tasks',)
+    # https://docs.celeryproject.org/en/latest/userguide/configuration.html
