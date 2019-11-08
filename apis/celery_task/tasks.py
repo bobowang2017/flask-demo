7563c5f91439e6316de8dd5f67fd046473c06cab
bobowang2017 <anini456123>
2019-11-08 11:25:51 +0800

    init

--- apis/celery_task/tasks.py
+++ apis/celery_task/tasks.py
@@ -8,9 +8,11 @@ logger = get_task_logger(__name__)
 @celery_app.task
 def timer_print():
     logger.info('*' * 50)
-    from apis.project.models import Project
-    projects = Project.query.all()
-    for p in projects:
-        logger.info(p.name)
-        logger.info(p.code)
-        logger.info('*' * 50)
+    from app import app
+    with app.app_context():
+        from apis.project.models import Project
+        projects = Project.query.all()
+        for p in projects:
+            logger.info(p.name)
+            logger.info(p.code)
+            logger.info('*' * 50)
