0d6a5ab0edce992a493a785490b3147e15e2570a
bobowang2017 <anini456123>
2019-11-08 12:15:05 +0800

    init

--- exts.py
+++ exts.py
@@ -26,16 +26,16 @@ config = CONFIG['local']
 
 
 # 初始化celery
-class MyCelery(Celery):
-    def now(self):
-        """Return the current time and date as a datetime."""
-        from datetime import datetime
-        return datetime.now(self.timezone)
+# class MyCelery(Celery):
+#     def now(self):
+#         """Return the current time and date as a datetime."""
+#         from datetime import datetime
+#         return datetime.now(self.timezone)
 
 
-celery_app = MyCelery('celery_app', broker='redis://127.0.0.1:6379/0')
+# celery_app = MyCelery('celery_app', broker='redis://127.0.0.1:6379/0')
 
-# celery_app = Celery('celery_app', broker='redis://127.0.0.1:6379/0')
+celery_app = Celery('celery_app')
 celery_app.config_from_object('celery_config.CeleryConfig')
 celery_app.conf.beat_schedule = {
     'add-every-30-seconds': {
