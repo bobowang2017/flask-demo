c3f368f0db09eea2033e987a20212ec86fadd64b
bobowang2017 <anini456123>
2020-05-27 16:50:09 +0800

    完善分布式锁

--- distribute_lock/lock.py
+++ distribute_lock/lock.py
@@ -36,6 +36,8 @@ class RedisLock(object):
 
     def release_lock(self):
         # 利用lua脚本解锁，保证下面的三个操作是一个原子
+        # 主要是怕误将其他客户端的锁解开。比如客户端A加锁，一段时间之后客户端A解锁，在进入unlock后执行jedis.del()
+        # 之前，锁突然过期了，此时客户端B尝试加锁成功，然后客户端A再执行del()方法，则将客户端B的锁给解除。
         lua = """      
         if  redis.call('get', KEYS[1]) == ARGV[1] 
             then
